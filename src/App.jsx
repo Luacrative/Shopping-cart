986aeb1b0f1dad4c29898ee142b1a12b5909f24d
Luacrative <afif317@gmail.com>
2024-03-23 23:45:29 -0700

    Router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import { useState } from "react"
-import "./App.css"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home.jsx"
+import Navbar from "./components/Navbar.jsx"
+import "./styles/App.css"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />
+    },
+    {
+        path: "/Home",
+        element: <Home />
+    }
+]);
+
 
 function App() {
     return (
-        <h1>App</h1>
+        <>
+            <Navbar />
+            <RouterProvider router={router} />
+        </>
     )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import styles from "./Navbar.module.css"
+
+const Navbar = () => {
+    return (
+        <div className={styles.bar}>
+            <h1 className={styles.logo}>Shopping Cart</h1>
+        </div>
+    )
+};
+
+export default Navbar; 
