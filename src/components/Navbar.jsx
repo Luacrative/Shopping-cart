95c8edf293063ce6176196ecc50195ca4800610c
Luacrative <afif317@gmail.com>
2024-03-23 23:56:25 -0700

    Implement basic navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
 import styles from "./Navbar.module.css"
+import cartImage from "../assets/cart.png"
+import searchImage from "../assets/search.png"
 
 const Navbar = () => {
     return (
         <div className={styles.bar}>
-            <h1 className={styles.logo}>Shopping Cart</h1>
+            <div className={styles.left}>
+                <h1 className={styles.logo}>Shopping Cart</h1>
+            </div>
+            <div className={styles.right}>
+                <img className={styles.icon} src={searchImage} />
+                <img className={styles.icon} src={cartImage} />
+            </div>
         </div>
     )
 };
