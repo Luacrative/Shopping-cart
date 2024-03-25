b98999887daa8df21e494602a36641405aeb2422
Luacrative <afif317@gmail.com>
2024-03-24 20:39:24 -0700

    Add header with splash image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,14 @@
+import styles from "../styles/Home.module.css"
+import homeSplash from "../assets/home_splash.jpg"
+
 const Home = () => {
-    return <h1>Home</h1>
+    return (
+        <>
+            <header className={styles.splash}>
+                <img className={styles.splashImage} src={homeSplash} />
+            </header>
+        </>
+    )
 };
 
 export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,18 @@
-import styles from "./Navbar.module.css"
+import styles from "../styles/Navbar.module.css"
 import cartImage from "../assets/cart.png"
 import searchImage from "../assets/search.png"
 
 const Navbar = () => {
     return (
-        <div className={styles.bar}>
+        <nav className={styles.bar}>
             <div className={styles.left}>
-                <h1 className={styles.logo}>Shopping Cart</h1>
+                <h1 className={styles.logo}>Home Store</h1>
             </div>
             <div className={styles.right}>
                 <img className={styles.icon} src={searchImage} />
                 <img className={styles.icon} src={cartImage} />
             </div>
-        </div>
+        </nav>
     )
 };
 
