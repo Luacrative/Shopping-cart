aa1a09d46e0cbaf503f6f7c7bd8d48d43ac91d7d
Luacrative <afif317@gmail.com>
2024-03-26 00:12:01 -0700

    Fetch items from API

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,31 @@
+import { useState, useEffect } from "react"
 import styles from "../styles/Home.module.css"
 import homeSplash from "../assets/home_splash.jpg"
 
 const Home = () => {
+    const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+
+        const fetchItems = async () => {
+            const fetchedItems = [];
+
+            for (let i = 1; i <= 4; i++) {
+                const response = await fetch(`https://fakestoreapi.com/products/${i}`)
+                const json = await response.json();
+
+                fetchedItems.push(json);
+            }
+
+            setItems(fetchedItems);
+            setLoading(false);
+        };
+
+        fetchItems();
+    }, []);
+
     return (
         <>
             <header className={styles.splash}>
@@ -11,6 +35,20 @@ const Home = () => {
                     <button className="standardButton">Shop now</button>
                 </div>
             </header>
+
+            <main className={styles.featured}>
+                <h1>Featured items</h1>
+
+                {loading && <h2>Loading items</h2>}
+                
+                <div className={styles.items}>
+                    {items.map((item, index) => (
+                        <div key={index}>
+                            <p>{item.title}</p>
+                        </div>
+                    ))}
+                </div>
+            </main>
         </>
     )
 };
