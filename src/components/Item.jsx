b15061937c48c722ca3ceb4935b039db6c7920e2
Luacrative <afif317@gmail.com>
2024-03-28 00:15:33 -0700

    Item controls design

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react"
 import styles from "../styles/Item.module.css"
 import "../styles/App.css"
 
 const Item = ({ curItem }) => {
+    const [quantity, setQuantity] = useState(0);
+
     return (
-        <main className={styles.container}>
+        <main className={styles.item}>
             <img className={styles.itemImage} src={curItem.image} alt="" />
             <div className={styles.itemInfo}>
                 <h2 className="itemTitle">{curItem.title}</h2>
                 <p className={styles.itemPrice}>${curItem.price}</p>
                 <p className={styles.itemDesc}>An amazing shopping item.</p>
                 <hr />
+
+                <div className={styles.itemControls}>
+                    <div className={styles.itemControlsQuantity}>
+                        <button className={styles.transparentButton}>-</button>
+                        <input type="number" className={styles.itemQuantityInput} />
+                        <button className={styles.transparentButton}>+</button>
+                    </div>
+
+                    <button className="standardButton">Add to cart</button>
+                    <button className={styles.transparentButton}>Back</button>
+                </div>
             </div>
         </main>
     )
