c5bede9e70ff87de92514144c1b1acee0f2c3155
Luacrative <afif317@gmail.com>
2024-03-28 00:03:05 -0700

    Display item information

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,11 +1,16 @@
 import styles from "../styles/Item.module.css"
+import "../styles/App.css"
 
-const Item = ({ curItem, setCurItem }) => {
-    console.log(curItem);
-
+const Item = ({ curItem }) => {
     return (
         <main className={styles.container}>
-            <h1>{curItem.title}</h1>
+            <img className={styles.itemImage} src={curItem.image} alt="" />
+            <div className={styles.itemInfo}>
+                <h2 className="itemTitle">{curItem.title}</h2>
+                <p className={styles.itemPrice}>${curItem.price}</p>
+                <p className={styles.itemDesc}>An amazing shopping item.</p>
+                <hr />
+            </div>
         </main>
     )
 };
